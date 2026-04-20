efca0cbc1eef853408a9ae041f1ed830902c5e7f
Juanse Ibarra <juanseibarra1@gmail.com>
2026-04-20 19:23:18 -0300

    TurnoControllerRest

--- src/main/java/com/veterinaria/veterinaria/controllerRest/TurnoControllerRest.java
+++ src/main/java/com/veterinaria/veterinaria/controllerRest/TurnoControllerRest.java
@@ -1,4 +1,39 @@
 package com.veterinaria.veterinaria.controllerRest;
 
+import com.veterinaria.veterinaria.dao.TurnoDAO;
+import com.veterinaria.veterinaria.model.Turno;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/turnos")
+
 public class TurnoControllerRest {
+    private TurnoDAO turnoDAO = new TurnoDAO(null);
+
+    @GetMapping
+    public List<Turno> obtenerTodosLosTurnos(){
+        return turnoDAO.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Turno obtenerTurnoPorId(@PathVariable("id") int id_turno){
+        return turnoDAO.obtenerPorID(id_turno);
+    }
+
+    @PostMapping
+    public String crearTurno(@RequestBody Turno turno){
+        turnoDAO.insertarTurno(turno.getMotivo(), turno.getId_cliente(), turno.getId_mascota());
+        return "Turno creado exitosamente";
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarTurno(@PathVariable("id") int id_turno){
+        turnoDAO.eliminarTurno(id_turno);
+        return "Turno eliminado exitosamente";
+    }
+
+
+
 }
