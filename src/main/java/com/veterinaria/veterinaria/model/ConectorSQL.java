7fd9235affc8b7a80694742ff5632d29dde1d0ce
federico <federicosola09@gmail.com>
2026-04-20 11:25:06 -0300

    clases modelo

--- src/main/java/com/veterinaria/veterinaria/model/ConectorSQL.java
+++ src/main/java/com/veterinaria/veterinaria/model/ConectorSQL.java
@@ -1,4 +1,21 @@
 package com.veterinaria.veterinaria.model;
 
-public class ConectorSQL {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectorSQL { //FALTA AGREGAR LOS PARAMETROS PASSWORD DBNAME Y URL
+    private static final String user = "root";
+    private static final String password = "";
+    private static final String db_name = "";
+    private static final String url = "";
+
+    public static Connection crearConexion() {
+        try {
+            return DriverManager.getConnection(url + db_name, user, password);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: \n" + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/com/veterinaria/veterinaria/model/Veterinario.java
+++ src/main/java/com/veterinaria/veterinaria/model/Veterinario.java
@@ -1,4 +1,77 @@
 package com.veterinaria.veterinaria.model;
 
 public class Veterinario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String matricula;
+    private String especialidad;
+    private String telefono;
+    private String direccion;
+
+    public Veterinario(int id, String nombre, String apellido, String matricula, String especialidad, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.especialidad = especialidad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
