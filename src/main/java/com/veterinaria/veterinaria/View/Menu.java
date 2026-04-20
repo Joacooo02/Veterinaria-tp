2ba368d821a114f31f4194176c0070213839a5fa
Joaquin Silva <joaquinsilva0284@gmail.com>
2026-04-20 18:02:44 -0300

    modifico algunas cosas del menu principal

--- src/main/java/com/veterinaria/veterinaria/View/Menu.java
+++ src/main/java/com/veterinaria/veterinaria/View/Menu.java
@@ -17,20 +17,40 @@ public class Menu {
 		this.menuMascota = new MenuMascota();
 		this.menuTurno = new MenuTurno();
 		this.menuVeterinario = new MenuVeterinario();
-
 	}
 
 	public void menu(){
 		int opcion;
 		do {
+			System.out.println("------------------------Bienvenido al Menu de la Veterinaria------------------------");
+			System.out.println("1 Gestion Mascotas");
+			System.out.println("2 Gestion Clientes");
+			System.out.println("3 Gestion Turnos");
+			System.out.println("4 Gestion Veterinarios");
+			System.out.println("5 Salir del programa");
+			System.out.println("Ingrese una opcion: ");
 			opcion = scanner.nextInt();
 			scanner.nextLine();
 			switch (opcion){
+				case 1:
+						menuMascota.menuMascota();
+					break;
+				case 2:
 
+					break;
+				case 3:
 
+					break;
+				case 4:
+
+					break;
+				case 5:
+					break;
+				default:
+					System.out.println("Opcion invalida, ingrese otra: ");
+					break;
 			}
-
-		}while (opcion != 0);
+		}while (opcion != 5);
 
 	}
 
